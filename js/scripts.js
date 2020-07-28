c65d51202e4fc7a40ed74537f13a4f049825721b
toysheeyeyraku <kirpich1337228@gmail.com>
2020-07-28 13:53:02 +0300

    pererob

--- js/scripts.js
+++ js/scripts.js
@@ -82,8 +82,32 @@ $(document).ready(function () {
   });
   
    
-  
+  /* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+        
 });
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("drop").classList.toggle("rotate");
+  }
+  $( ".choose" ).click(function() {
+      var val =$(this).html();
+      $(this).parent().parent().find('.dropbtn').html(val);
+  });
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+          document.getElementById("drop").classList.toggle("rotate");
+        }
+      }
+    }
+  }
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
